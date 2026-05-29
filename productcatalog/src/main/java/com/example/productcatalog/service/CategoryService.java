36b07b4ba20ac39deca3aa62e071129d09db1680
Jvivekvardhan <vivekverdhan47@gmail.com>
2026-05-29 13:20:10 +0530

    Day 2 Added controllers services and repositories

---
+++ productcatalog/src/main/java/com/example/productcatalog/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.example.productcatalog.service;
+
+import com.example.productcatalog.model.Category;
+import com.example.productcatalog.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
